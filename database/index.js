1396ed3d351fdd7d1d205ea300f6b5a719917c26
Mottaaaa <pedro10mota@msn.com>
2020-12-19 19:18:07 +0000

    db connection timeout fix

--- database/index.js
+++ database/index.js
@@ -2,7 +2,15 @@ const sql = require('mysql');
 let config = require('./config.json');
 
 var connection = sql.createConnection(config);
-connection.connect();
+connection.connect(err => {
+    if (err) console.log(`Error connecting to database: ${err}`);
+    else console.log(`Connected to database`);
+});
+
+connection.on('error', (err) => {
+    if(err === 'PROTOCOL_CONNECTION_LOST') connection.connect();
+    else throw err;
+});
 
 module.exports = {
     ...require('./getaride')(connection),
