7b967bc78224c02ed1eabe670a48116d62c530b2
Rick2323 <Ricardo.C.Professional@hotmail.com>
2021-01-20 11:48:31 +0000

    fix

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
-const app = require('./index');
+/*const app = require('./index');
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log('Server listening on port: ' + port);
-});
+});*/
+
+const express = require("express");
+const path = require("path");
+const config = require("config");
+
+const app = express();
+
+// Body-parser Middleware
+app.use(express.json());
+// Use Routes
+app.use('/api', require('./router/api'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
